6bfc05e8e7b6a6f57a19a44263c47a05d8ab6f75
MrExceptionist <pureexception@gmail.com>
2020-12-12 12:41:55 +0530

    First Commit

---
+++ src/test/java/com/example/Package1/ExpensesSharingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Package1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesSharingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
